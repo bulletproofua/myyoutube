baa3d0a87520eb315e7632193e1d5a453c2815f3
bulletproofua <yuriy.haydeychuk@gmail.com>
2017-03-23 10:33:03 +0200

    bootstrap C

--- controllers/dateFormat.js
+++ controllers/dateFormat.js
@@ -1,7 +1,9 @@
 exports.dataFormat = function(data){    					
         var date = new Date(data);
-        var format = date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate()+"  "+ date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+        var format = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds()+"  "+ date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
         console.log( format )
     return format;
 } 
 
+
+      
